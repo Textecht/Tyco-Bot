d32a1f68966847b6d68377361acb65398f419a85
Textecht <124495970+Textecht@users.noreply.github.com>
2023-02-05 12:58:12 +0300

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -20,14 +20,14 @@ export default function AwardApp({ Component, pageProps }) {
   const NavItems = [
     {
       link: true,
-      name: "Home",
+      name: "Ana Sayfa",
       icon: "fal fa-home",
       activeIcon: "fa fa-home",
       href: "/",
     },
     {
       link: true,
-      name: "Commands",
+      name: "Komutlar",
       icon: "fa fa-list-alt",
       activeIcon: "fa fa-list-alt",
       href: "/commands",
